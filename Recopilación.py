90dbd7a01d57b53217b09a2efaa178efc5048baf
Jesús Vega <74364473+jchicov@users.noreply.github.com>
2022-11-22 21:46:47 +0100

    Update Recopilación.py

--- Recopilación.py
+++ Recopilación.py
@@ -1,6 +1,7 @@
 #Aquí recopilaré todo el código que aprenda o se me ocurra y que me resulte útli para el Data Science
 import numpy as np
 
+#Calcula la media de una lista de valores
 def mean_lst(lst):
   sum = 0
   l = len(lst)
@@ -9,6 +10,7 @@ def mean_lst(lst):
   m = sum/l
   return(m)
 
+#Calcula la varianza de una lista de valores
 def var_lst(lst):
   media = mean(lst)
   l = []
@@ -17,6 +19,7 @@ def var_lst(lst):
   v = mean(l)
   return(v)
 
+#Calcula la Desviación Típica (Standart Deviation) de una lista de valores
 def sd_lst(lst):
   sd = (var(lst))**(0.5)
   return(sd)
